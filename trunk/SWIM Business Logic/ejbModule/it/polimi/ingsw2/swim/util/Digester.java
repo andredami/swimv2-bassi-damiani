afc10c0254b8364e67932b8c7de80e590216d61e
andreadamiani90@gmail.com <andreadamiani90@gmail.com@73279aa5-3323-d202-c4c8-c7b282fda374>
2013-01-21 05:00:51 +0000

    correzione algoritmo di codifica delle password

--- trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/util/Digester.java
+++ trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/util/Digester.java
@@ -15,7 +15,7 @@ public abstract class Digester {
 
 	private static MessageDigest digestProvider = null;
 
-	private static final String securityAlg = "AES";
+	private static final String securityAlg = "MD5";
 	
 	static{
 		try {
@@ -28,8 +28,19 @@ public abstract class Digester {
 	
 	public static synchronized String digest(String s){
 		if(digestProvider != null){
-			byte[] digested = digestProvider.digest(s.getBytes());
-			return digested.toString();
+			byte[] plainText = s.getBytes();
+			digestProvider.reset();
+			digestProvider.update(plainText);
+			byte[] encodedPassword = digestProvider.digest();
+
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < encodedPassword.length; i++) {
+				if ((encodedPassword[i] & 0xff) < 0x10) {
+					sb.append("0");
+				}
+				sb.append(Long.toString(encodedPassword[i] & 0xff, 16));
+			}
+			return sb.toString();
 		} else {
 			Logger.getLogger("SECURITY").log(java.util.logging.Level.WARNING, "The " + securityAlg + " algorithm is not available. All passwords will be stored in plain-text!");
 			return s;
