1b209fd921fe1f2d700aa5a97b80bdf94348e763
ilpillo90@gmail.com <ilpillo90@gmail.com@73279aa5-3323-d202-c4c8-c7b282fda374>
2013-01-21 09:42:42 +0000

    verifica se il progetto ha problemi con le versioni java

---
+++ SWIM Front End/src/it/polimi/ingsw2/swim/pages/LoginServlet.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw2.swim.pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Io sono un titolo!</title>");
+        out.println("</head>");
+        out.println("<body bgcolor=\"white\">");
+        out.println("<h1>Questa è una prova!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+	}
+
+}
