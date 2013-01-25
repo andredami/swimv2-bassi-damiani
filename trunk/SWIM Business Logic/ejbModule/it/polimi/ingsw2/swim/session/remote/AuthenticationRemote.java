0613514f0ebae96e8fb868a4ef1c5c1168f1e226
andreadamiani90@gmail.com <andreadamiani90@gmail.com@73279aa5-3323-d202-c4c8-c7b282fda374>
2013-01-25 12:46:09 +0000

    Corretti metodi esposti dalle interfacce

--- trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/session/remote/AuthenticationRemote.java
+++ trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/session/remote/AuthenticationRemote.java
@@ -6,5 +6,7 @@ import it.polimi.ingsw2.swim.exceptions.NoSuchUserException;
 public interface AuthenticationRemote {
 
 	public User authenticate(String email, String password) throws NoSuchUserException;
+
+	void generateTemporaryPassword(String email) throws NoSuchUserException;
 	
 }
--- trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/session/remote/FriendshipManagerRemote.java
+++ trunk/SWIM Business Logic/ejbModule/it/polimi/ingsw2/swim/session/remote/FriendshipManagerRemote.java
@@ -1,5 +1,18 @@
 package it.polimi.ingsw2.swim.session.remote;
 
+import it.polimi.ingsw2.swim.exceptions.InvalidDataException;
+import it.polimi.ingsw2.swim.exceptions.NoSuchUserException;
+
 public interface FriendshipManagerRemote {
+
+	void confirm(String userId, String friendshipId)
+			throws InvalidDataException;
+
+	void deny(String userId, String friendshipId) throws InvalidDataException;
+
+	void remove(String userId, String friendToBeRemovedId)
+			throws NoSuchUserException;
+
+	void ask(String addresseeId, String senderId) throws NoSuchUserException;
 
 }
