f069c51b947c2bbd846f5cded1112dcf9e734716
ilpillo90@gmail.com <ilpillo90@gmail.com@73279aa5-3323-d202-c4c8-c7b282fda374>
2013-01-25 23:58:59 +0000

    siamo sulla buona strada

--- SWIM Front End/src/it/polimi/ingsw2/swim/pages/SelectAbilityRegistrationServlet.java
+++ SWIM Front End/src/it/polimi/ingsw2/swim/pages/SelectAbilityRegistrationServlet.java
@@ -2,10 +2,12 @@ package it.polimi.ingsw2.swim.pages;
 
 
 import it.polimi.ingsw2.swim.entities.Ability;
+import it.polimi.ingsw2.swim.session.remote.AbilityManagerRemote;
 import it.polimi.ingsw2.swim.session.remote.AbilitySearchRemote;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -39,26 +41,14 @@ public class SelectAbilityRegistrationServlet extends HttpServlet {
 		try {
 			// Starting the context
 			InitialContext jndiContext = new InitialContext();
-			Object ref = jndiContext.lookup("AbilitySearch/remote");
-			AbilitySearchRemote a = (AbilitySearchRemote) ref; 
+			Object ref = jndiContext.lookup("AbilityManager/remote");
+			AbilityManagerRemote a = (AbilityManagerRemote) ref; 
 			
-			// if the ability name is not specified, return with an error message
-			if (request.getParameter("TextAbility").isEmpty()){
-				request.getSession().setAttribute("EmptyRequest", 1);
-				response.sendRedirect(request.getContextPath() + "/Pages/AbilitySelection.jsp");
-				return;
-			}
-			String abilityRequested = request.getParameter("TextAbility");
-			// if there is no ability with that name, return a notification
-			if (a.findAbility(abilityRequested).isEmpty()){
-				request.getSession().setAttribute("NotFoundAbility", 1);
-				response.sendRedirect(request.getContextPath() + "/Pages/AbilitySelection.jsp");
-				return;
-			}
-				
-			// set the attribute for the jsp page
-			request.getSession().setAttribute("AbilityList", a.findAbility(abilityRequested));
-			response.sendRedirect(request.getContextPath() + "/Pages/AbilitySelection.jsp");
+			// load all the abilities from the database
+			List<Ability> abilityList = a.retriveAbilityList();
+			request.getSession().setAttribute("abilityList", abilityList);
+			String url = response.encodeURL("/Pages/AbilitySelection.jsp");
+			response.sendRedirect(request.getContextPath() + url);
 			return;
 			
 		} catch (NamingException e) {
